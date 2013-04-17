ab55c428dd8a3aa4c80ec6146986c4b3094b89ce
Tanner Rogalsky <the.t.dawg@gmail.com>
2013-04-17 14:51:26 -0400

    update colors lib

--- lib/colors.lua
+++ lib/colors.lua
@@ -15,6 +15,62 @@ function color_map_mt.rgba(color)
   return color.r, color.g, color.b, color.a or 255
 end
 
+function color_map_mt.rgb_to_hsl(rgb_color)
+  local r,g,b = rgb_color:rgb()
+  r = r / 255
+  g = g / 255
+  b = b / 255
+
+  local max, min = math.max(r, g, b), math.min(r, g, b)
+  local average = (max + min) / 2
+  local h,s,l = average,average,average
+
+  if min == max then
+    h,s = 0,0 -- achromatic
+  else
+    local d = max - min
+    s = l > 0.5 and d / (2 - max - min) or d / (max + min)
+
+    if max == r then
+      h = (g - b) / d + (g < b and 6 or 0)
+    elseif max == g then
+      h = (b - r) / d + 2
+    elseif max == b
+      h = (r - g) / d + 4
+    end
+
+    h = h / 6
+  end
+
+  return h, s, l
+end
+
+function color_map_mt.hsl_to_rgb(h, s, l)
+  local r,g,b
+
+  if s == 0 then
+    r,g,b = l,l,l -- achromatic
+  else
+    local function hue_to_rgb(p, q, t)
+      if t < 0 then t = t + 1 end
+      if t > 1 then t = t - 1 end
+      if t < 1/6 then return p + (q - p) * 6 * t end
+      if t < 1/2 then return q end
+      if t < 2/3 then return p + (q - p) * (2/3 - t) * 6 end
+      return p
+    end
+
+    local q = l < 0.5 and l * (1 + s) or l + s - l * s
+    local p = 2 * l - q
+    r = hue2rgb(p, q, h + 1/3)
+    g = hue2rgb(p, q, h)
+    b = hue2rgb(p, q, h - 1/3)
+  end
+
+  return r * 255, g * 255, b * 255
+end
+
+
 -- metatable functions for the encompassing colors collection
 function colors_mt.__newindex(t, k, v)
   setmetatable(v, color_map_mt)
