365945dab96df0b67733eddcb6e7201d11322093
Tanner Rogalsky <the.t.dawg@gmail.com>
2013-12-17 08:26:23 -0500

    add concat metamethod to the base class

--- base.lua
+++ base.lua
@@ -23,3 +23,7 @@ end
 function Base:__tostring()
   return "Instance of ".. self.class.name .." with ID ".. self.id
 end
+
+function Base.__concat(value1, value2)
+  return tostring(value1) .. tostring(value2)
+end
