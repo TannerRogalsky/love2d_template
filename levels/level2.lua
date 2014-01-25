b58de43d82b4265208ea5732f7d76afe167d2636
Tanner Rogalsky <tanner@tannerrogalsky.com>
2014-01-25 15:10:11 -0500

    draw obstructions

--- levels/level2.lua
+++ levels/level2.lua
@@ -5,16 +5,16 @@ return {
   height = height,
   obstructions = {
     {
-      geometry = {width / 4, height / 4, 50, 100}
+      geometry = {width / 4, height / 4, 50, 50}
     },
     {
-      geometry = {width / 4, height / 4 * 3, 50, 100}
+      geometry = {width / 4, height / 4 * 3, 50, 50}
     },
     {
-      geometry = {width / 4 * 3, height / 4 * 3, 50, 100}
+      geometry = {width / 4 * 3, height / 4 * 3, 50, 50}
     },
     {
-      geometry = {width / 4 * 3, height / 4, 50, 100}
+      geometry = {width / 4 * 3, height / 4, 50, 50}
     },
   },
   traps = {
--- obstruction.lua
+++ obstruction.lua
@@ -1,7 +1,12 @@
 Obstruction = class('Obstruction', Base):include(Stateful)
+Obstruction.static.OVERDRAW_X = 1.2
+Obstruction.static.OVERDRAW_Y = 1.2
 
 function Obstruction:initialize(x, y, w, h)
   Base.initialize(self)
+
+  self.image = game.preloaded_images["flesh_tube.png"]
+  self.width, self.height = w, h
 
   self.body = love.physics.newBody(World, x, y, "static")
   self.shape = love.physics.newRectangleShape(w, h)
@@ -13,6 +18,13 @@ function Obstruction:update(dt)
 end
 
 function Obstruction:render()
-  g.setColor(COLORS.grey:rgb())
-  g.polygon("fill", self.body:getWorldPoints(self.shape:getPoints()))
+  -- g.setColor(COLORS.grey:rgb())
+  -- g.polygon("fill", self.body:getWorldPoints(self.shape:getPoints()))
+
+  g.setColor(COLORS.white:rgb())
+  local x, y = self.body:getWorldCenter()
+  local iw, ih = self.image:getWidth(), self.image:getHeight()
+  local sx, sy = self.width / iw, self.height / ih
+  sx, sy = sx * Obstruction.OVERDRAW_X, sy * Obstruction.OVERDRAW_Y
+  g.draw(self.image, x, y, 0, sx, sy, self.width / sx / 2, self.height / sy / 2)
 end
