8aa6ba48d2e4ecbd72e0899f5b4cb7913b89c3a0
Tanner Rogalsky <tanner@tannerrogalsky.com>
2014-01-26 13:05:48 -0500

    trap lord

--- trap.lua
+++ trap.lua
@@ -4,29 +4,25 @@ Trap.static.SIZE_INCREASE = 1.5
 function Trap:initialize(x, y, w, h)
   Base.initialize(self)
 
+  self.image = game.preloaded_images["trap.png"]
+  self.width, self.height = w * Trap.SIZE_INCREASE, h * Trap.SIZE_INCREASE
+
   self.body = love.physics.newBody(World, x, y, "kinematic")
-  self.shape = love.physics.newRectangleShape(w, h)
+  self.shape = love.physics.newCircleShape(w / 2)
   self.fixture = love.physics.newFixture(self.body, self.shape)
   self.fixture:setUserData(self)
   self.fixture:setSensor(true)
-
-  -- we want slightly larger box to render than the collision box
-  self.render_vertices = {}
-  local points = {self.shape:getPoints()}
-  for _,vertex in ipairs(points) do
-    table.insert(self.render_vertices, vertex * Trap.SIZE_INCREASE)
-  end
 end
 
 function Trap:update(dt)
 end
 
 function Trap:render()
-  g.setColor(COLORS.red:rgb())
-  g.polygon("fill", self.body:getWorldPoints(unpack(self.render_vertices)))
-
-  g.setColor(COLORS.black:rgb())
-  g.polygon("fill", self.body:getWorldPoints(self.shape:getPoints()))
+  local x, y = self.body:getWorldCenter()
+  local iw, ih = self.image:getWidth(), self.image:getHeight()
+  local sx, sy = self.width / iw, self.height / ih
+  g.setColor(COLORS.white:rgb())
+  g.draw(self.image, x, y, 0, sx, sy, self.width / sx / 2, self.height / sy / 2)
 end
 
 function Trap:begin_contact(other, contact)
