84f701621060797caa8b599f19c44f16d9605133
Tanner Rogalsky <the.t.dawg@gmail.com>
2014-01-27 21:54:50 -0500

    remove console window on windows

--- conf.lua
+++ conf.lua
@@ -2,7 +2,7 @@ function love.conf(t)
   t.author = "Tanner Rogalsky"
   t.identity = "love2d_template"                   -- The name of the save directory (string)
   t.version = "0.9.0"                -- The LÖVE version this game was made for (string)
-  t.console = true                  -- Attach a console (boolean, Windows only)
+  t.console = false                  -- Attach a console (boolean, Windows only)
 
   t.window.title = "Template"        -- The window title (string)
   t.window.icon = nil                -- Filepath to an image to use as the window's icon (string)
