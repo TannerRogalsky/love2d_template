0b984cc4fcd621d6d4f27650458102cb244f1fc3
Tanner Rogalsky <tanner@tannerrogalsky.com>
2014-04-25 22:15:29 -0400

    fix travis

--- levels/triggers/level1.lua
+++ levels/triggers/level1.lua
@@ -1,15 +1,11 @@
 local triggers = {}
 
-function triggers.test_enter(object_one, object_two, contact, nx, ny, ...)
-  print("enter")
-  print(object_one, object_two, contact, nx, ny, ...)
-  object_two.body:applyLinearImpulse(0, -60)
-  object_two.can_jump = false
+function triggers.test_enter(trigger, object, contact, nx, ny, ...)
+  object.body:applyLinearImpulse(0, -60)
+  object.can_jump = false
 end
 
-function triggers.test_exit(object_one, object_two, contact, nx, ny, ...)
-  print("exit")
-  print(object_one, object_two, contact, nx, ny, ...)
+function triggers.test_exit(trigger, object, contact, nx, ny, ...)
 end
 
 return triggers
