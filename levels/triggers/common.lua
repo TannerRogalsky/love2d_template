b6ac19003d014b62fbd586494271d09d8d021d7a
Tanner Rogalsky <tanner@tannerrogalsky.com>
2014-04-26 18:46:36 -0400

    remove common triggers that have been replaced with the goal class

--- levels/triggers/common.lua
+++ levels/triggers/common.lua
@@ -1,18 +1,5 @@
 local triggers = {}
 
-function triggers.goal_enter(goal, player)
-  if goal.player and goal.player == player.player_name then
-    goal.triggered = true
-    print(goal.player, goal.triggered)
-  end
-end
-
-function triggers.goal_exit(goal, player)
-  if goal.player and goal.player == player.player_name then
-    goal.triggered = false
-    print(goal.player, goal.triggered)
-  end
-end
 
 -- removes the coin from the level
 function triggers.coin_enter(coin, object)
