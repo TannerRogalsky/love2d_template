8945afcd34a6efc3e4f993e1763d5ead6ca71539
Tanner Rogalsky <tanner@tannerrogalsky.com>
2014-04-26 20:33:40 -0400

    add random offset to bounce animation

--- levels/triggers/common.lua
+++ levels/triggers/common.lua
@@ -1,11 +1,13 @@
 local triggers = {}
 
 function triggers.bounce_animation(trigger, dt)
+  local offset = trigger.offset or math.random() * (math.pi / 2)
+  trigger.offset = offset
   local layer = trigger.tile_layer or "Background"
   local frequency = trigger.frequency or 3
   local amplitude = trigger.amplitude or 21 / 4
   local x, y = trigger.body:getWorldCenter()
-  y = y + math.sin(love.timer.getTime() * frequency) * amplitude
+  y = y + math.sin((love.timer.getTime() + offset)  * frequency) * amplitude
   local sprite_id = level.tile_layers[layer].sprite_lookup:get(trigger.tile_x, trigger.tile_y)
   local quad = level.tile_layers[layer].quad_lookup:get(trigger.tile_x, trigger.tile_y)
   level.tile_layers[layer].sprite_batch:set(sprite_id, quad, x, y)
