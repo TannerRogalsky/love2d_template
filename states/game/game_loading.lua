70433cdff0bc65a3fd2d3e65de2152e60e64b509
Tanner Rogalsky <tanner@tannerrogalsky.com>
2014-04-27 12:27:53 -0400

    full screen!

--- states/game/game_loading.lua
+++ states/game/game_loading.lua
@@ -34,7 +34,7 @@ function Loading:enteredState()
     -- loader finished callback
     -- initialize game stuff here
 
-    -- love.window.setFullscreen(true, "desktop")
+    love.window.setFullscreen(true, "desktop")
     self:gotoState("Title")
   end)
 end
