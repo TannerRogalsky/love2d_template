e4c44afc4fbfe0eedb660507a623c101cb8d7d8f
Tanner Rogalsky <tanner@tannerrogalsky.com>
2014-04-27 12:28:33 -0400

    resolution independent menu

--- states/game/game_menu.lua
+++ states/game/game_menu.lua
@@ -1,6 +1,9 @@
 local Menu = Game:addState('Menu')
 
 function Menu:enteredState()
+  self.menu_font = g.newFont("fonts/04b03.TTF", 48)
+  g.setFont(self.menu_font)
+
   self.sorted_names = {}
   for name,_ in pairs(self.preloaded_levels) do
     table.insert(self.sorted_names, name)
@@ -9,14 +12,15 @@ function Menu:enteredState()
 
   self.all_levels = {}
   for _, name in pairs(self.sorted_names) do
-    local level = g.newCanvas(400, 400)
+    local level = g.newCanvas()
     g.setCanvas(level)
     g.setColor(COLORS.cornflowerblue:rgb())
-    g.rectangle("fill", 0, 0, 400, 400)
+    g.rectangle("fill", 0, 0, level:getWidth(), level:getHeight())
     g.setColor(COLORS.white:rgb())
-    local tile_layers = MapLoader.load(name).tile_layers
-    g.draw(tile_layers["Background"].sprite_batch, 0, 0, 0, 0.5, 0.5)
-    g.draw(tile_layers["Foreground"].sprite_batch, 0, 0, 0, 0.5, 0.5)
+    local level_data = MapLoader.load(name)
+    local tile_layers = level_data.tile_layers
+    g.draw(tile_layers["Background"].sprite_batch, 0, 0, 0, level_data.scale, level_data.scale)
+    g.draw(tile_layers["Foreground"].sprite_batch, 0, 0, 0, level_data.scale, level_data.scale)
     g.setCanvas()
     self.all_levels[name] = level
   end
@@ -26,23 +30,23 @@ function Menu:enteredState()
 end
 
 function Menu:draw()
+  g.setColor(COLORS.white:rgb())
+  g.draw(self.all_levels[self.sorted_names[self.selected_level_index]], 0, 0)
+
   local dy = love.graphics.getFont():getHeight() + 3
   for index,name in ipairs(self.sorted_names) do
     local x, y = 5, index * dy
-    g.setColor(COLORS.white:rgb())
-    g.print(name, x, y)
+    g.setColor(COLORS.black:rgb())
+    g.printf(name, 0, y, g.getWidth(), "center")
 
     if index == self.selected_level_index then
       g.setColor(COLORS.yellow:rgb())
       local old_width = g.getLineWidth()
       g.setLineWidth(2)
-      g.rectangle("line", x - 2, y - 2, g.getFont():getWidth(name) + 4, dy)
+      g.rectangle("line", g.getWidth() / 3, y - 2, g.getWidth() / 3, dy)
       g.setLineWidth(old_width)
     end
   end
-
-  g.setColor(COLORS.white:rgb())
-  g.draw(self.all_levels[self.sorted_names[self.selected_level_index]], 200, 100)
 end
 
 function Menu:keypressed(key, unicode)
