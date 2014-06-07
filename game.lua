e41530e5329d5c48aae5ac8ac6a30b68d705678d
Tanner Rogalsky <tanner@tannerrogalsky.com>
2014-06-07 12:32:31 -0400

    wip

--- game.lua
+++ game.lua
@@ -12,7 +12,58 @@ function Game:initialize(args)
     love.graphics.rectangle("fill", x, y, 1, 1)
   end
 
+  sharecart_data = self:load_sharecart_data()
+  self:save_sharecart_data(sharecart_data)
+
   self:gotoState("Loading")
+end
+
+local sharecart_keys = {
+  "MapX",
+  "MapY",
+  "Misc0",
+  "Misc1",
+  "Misc2",
+  "Misc3",
+  "PlayerName",
+  "Switch0",
+  "Switch1",
+  "Switch2",
+  "Switch3",
+  "Switch4",
+  "Switch5",
+  "Switch6",
+  "Switch7",
+}
+
+function Game:load_sharecart_data()
+  local data = {}
+  local file = io.open("../dat/o_o.ini")
+  for line in file:lines() do
+    local key, value = line:match("(%w+)=(.*)")
+    if key and value then
+      if value:upper() == "TRUE" then
+        data[key] = true
+      elseif value:upper() == "FALSE" then
+        data[key] = false
+      else
+        data[key] = tonumber(value) or value
+      end
+    end
+  end
+  file:close()
+  return data
+end
+
+function Game:save_sharecart_data(data)
+  local file = io.open("../dat/o_o.ini", "w+")
+  local output = "[Main]"
+  for _, key in ipairs(sharecart_keys) do
+    local value = data[key]
+    output = output .. string.format("\n%s=%s", key, value)
+  end
+  file:write(output)
+  file:close()
 end
 
 function Game:update(dt)
