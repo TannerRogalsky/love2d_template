20a9ed3e617b3d46276f2a603ae3e6b4852d4ad4
Tanner Rogalsky <tanner@tannerrogalsky.com>
2014-08-10 15:56:43 -0400

    predator chase mode

--- predator.lua
+++ predator.lua
@@ -13,7 +13,7 @@ function Predator:initialize(section, origin)
   self.section = section
   self.origin = origin:clone()
   self.x, self.y = self.origin.x, self.origin.y
-  self.ox, self.oy = math.random(3), math.random(3)
+  self.ox, self.oy = 1, 1
   self.image = game.preloaded_images["predator.png"]
   self.last_x, self.last_y = self.x, self.y
   self.angle = 0
@@ -25,12 +25,29 @@ function Predator:initialize(section, origin)
 end
 
 function Predator:update(dt)
-  self.dt = self.dt + dt
-  self.angle = math.atan2(self.last_y - self.y, self.last_x - self.x) - math.pi / 2
-  self.last_x, self.last_y = self.x, self.y
-  self.x = self.origin.x + math.cos(self.dt) * (Predator.SPEED / self.ox)
-  self.y = self.origin.y + math.sin(self.dt) * (Predator.SPEED / self.oy)
+  local closest_alpha = self:get_closest_alpha()
+  local target_pos
+  local pos = Vector(self.x, self.y)
+  if closest_alpha then
+    -- chase mode
+    target_pos = closest_alpha.position
+  else
+    self.dt = self.dt + dt
+    local x = self.origin.x + math.cos(self.dt) * (Predator.SPEED / self.ox)
+    local y = self.origin.y + math.sin(self.dt) * (Predator.SPEED / self.oy)
+    target_pos = Vector(x, y)
+  end
+
+  local self_pos = Vector(self.x, self.y)
+  delta = (target_pos - self_pos):normalized() * Predator.SPEED * dt
+  self.x, self.y = (self_pos + delta):unpack()
   self._physics_body:moveTo(self.x, self.y)
+
+  local new_angle = math.atan2(self.last_y - self.y, self.last_x - self.x) - math.pi / 2
+  -- local angle_delta = math.clamp(-math.pi / 8, new_angle - self.angle, math.pi / 8)
+  local angle_delta = new_angle - self.angle
+  self.angle = self.angle + angle_delta
+  self.last_x, self.last_y = self.x, self.y
 end
 
 function Predator:draw()
@@ -53,6 +70,20 @@ end
 
 function Predator:die()
   self:destroy()
+end
+
+function Predator:get_closest_alpha()
+  local closest = nil
+  local closest_dist = math.huge
+  local self_pos = Vector(self.x, self.y)
+  for _, alpha in pairs(self.section.alphas) do
+    local dist = self_pos:dist(alpha.position)
+    if dist < closest_dist then
+      closest_dist = dist
+      closest = alpha
+    end
+  end
+  return closest
 end
 
 function Predator:destroy()
