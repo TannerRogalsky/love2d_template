0d0a58881a293074ca6b3b544260aa4dcc0b3bc0
Tanner Rogalsky <tanner@tannerrogalsky.com>
2016-01-06 10:52:39 -0500

    remove things that are defaults now

--- requirements.lua
+++ requirements.lua
@@ -5,8 +5,6 @@ math.tau = math.pi * 2
 
 -- The pixel grid is actually offset to the center of each pixel. So to get clean pixels drawn use 0.5 + integer increments.
 g.setPointSize(2.5)
-g.setPointStyle("rough")
-math.randomseed(os.time())
 function math.round(n, deci) deci = 10^(deci or 0) return math.floor(n*deci+.5)/deci end
 function math.clamp(low, n, high) return math.min(math.max(low, n), high) end
 function pointInCircle(circle, point) return (point.x-circle.x)^2 + (point.y - circle.y)^2 < circle.radius^2 end
