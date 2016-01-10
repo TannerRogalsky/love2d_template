94ed7a56261dc54a5becf182875b6a4d08c8dc6e
Tanner Rogalsky <tanner@tannerrogalsky.com>
2016-01-10 14:31:59 -0500

    remove (incorrect) HC setup from template

--- states/game/game_main.lua
+++ states/game/game_main.lua
@@ -1,8 +1,6 @@
 local Main = Game:addState('Main')
 
 function Main:enteredState()
-  Collider = HC(100, self.on_start_collide, self.on_stop_collide)
-
   local Camera = require("lib/camera")
   self.camera = Camera:new()
 
@@ -39,27 +37,8 @@ end
 function Main:focus(has_focus)
 end
 
--- shape_one and shape_two are the colliding shapes. mtv_x and mtv_y define the minimum translation vector,
--- i.e. the direction and magnitude shape_one has to be moved so that the collision will be resolved.
--- Note that if one of the shapes is a point shape, the translation vector will be invalid.
-function Main.on_start_collide(dt, shape_one, shape_two, mtv_x, mtv_y)
-  local object_one, object_two = shape_one.parent, shape_two.parent
-
-  if object_one and is_func(object_one.on_collide) then
-    object_one:on_collide(dt, object_two, mtv_x, mtv_y)
-  end
-
-  if object_two and is_func(object_two.on_collide) then
-    object_two:on_collide(dt, object_one, -mtv_x, -mtv_y)
-  end
-end
-
-function Main.on_stop_collide(dt, shape_one, shape_two)
-end
-
 function Main:exitedState()
-  Collider:clear()
-  Collider = nil
+  self.camera = nil
 end
 
 return Main
