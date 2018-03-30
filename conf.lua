d5290f321e84986df166ee53b1f75241cc023ade
Tanner Rogalsky <tanner@tannerrogalsky.com>
2018-03-30 07:23:10 -0400

    update version

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
   t.identity = "love2d_template"      -- The name of the save directory (string)
-  t.version = "0.10.1"                -- The LÖVE version this game was made for (string)
+  t.version = "0.11.0"                -- The LÖVE version this game was made for (string)
   t.console = false                   -- Attach a console (boolean, Windows only)
   t.accelerometerjoystick = true      -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
   t.gammacorrect = false              -- Enable gamma-correct rendering, when supported by the system (boolean)
